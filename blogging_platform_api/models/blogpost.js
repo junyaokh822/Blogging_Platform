8b79f884e3f98c0f9db4c3b5ffba2cf474d8ee71
Junyao <junyaokh822@gmail.com>
2023-07-13 15:09:27 -0400

    create a blogpost model (model and migration)

---
+++ blogging_platform_api/models/blogpost.js
@@ -0,0 +1,25 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class BlogPost extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  BlogPost.init({
+    title: DataTypes.STRING,
+    contents: DataTypes.STRING,
+    postDate: DataTypes.DATE
+  }, {
+    sequelize,
+    modelName: 'BlogPost',
+  });
+  return BlogPost;
+};
