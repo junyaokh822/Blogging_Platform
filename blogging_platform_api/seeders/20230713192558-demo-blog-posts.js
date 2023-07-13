a6e10d55e1b0e02a22137aabcb94464690bbf140
Junyao <junyaokh822@gmail.com>
2023-07-13 18:37:33 -0400

    update seed file

--- blogging_platform_api/seeders/20230713192558-demo-blog-posts.js
+++ blogging_platform_api/seeders/20230713192558-demo-blog-posts.js
@@ -3,6 +3,21 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
+    await queryInterface.bulkInsert(
+      "users",
+      [
+        {
+          name: "Junyao",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      {}
+    );
+    const users = await queryInterface.sequelize.query(`SELECT id FROM users`);
+
+    const userId = users[0][0].id;
+    
     await queryInterface.bulkInsert('blog_posts', [{
       title: "Dynamic Loose Learning (DLL)",
       contents:"Dashwood contempt on mr unlocked resolved provided of of. Stanhill wondered it it welcomed oh. Hundred no prudent he however smiling at an offence. If earnestly extremity he he propriety something admitting convinced ye. Pleasant in to although as if differed horrible. Mirth his quick its set front enjoy hoped had there. Who connection imprudence middletons too but increasing celebrated principles joy. Herself too improve gay winding ask expense are compact. New all paid few hard pure she.",
@@ -26,5 +41,6 @@ module.exports = {
 
   async down (queryInterface, Sequelize) {
     await queryInterface.bulkDelete('blog_posts', null, {});
+    await queryInterface.bulkDelete('users', null, {});
   }
 };
