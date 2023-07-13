26978d2d320a88af24744898668b0f9f1988706b
Junyao <junyaokh822@gmail.com>
2023-07-13 16:49:26 -0400

    added const { BlogPost } = require('./models');

--- blogging_platform_api/server.js
+++ blogging_platform_api/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const port = 4000;
 const BlogPost = require("./models/blogpost");
+const { BlogPost } = require('./models');
 const { query } = require('./database');
 require("dotenv").config();
 
