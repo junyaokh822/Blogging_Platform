3c472f5b032b5fe8ede8499d3a1cc7b380fffef8
Junyao <junyaokh822@gmail.com>
2023-07-14 16:25:30 -0400

    added login func for server.js

--- blogging_platform_api/server.js
+++ blogging_platform_api/server.js
@@ -20,6 +20,8 @@ app.get("/", (req, res) => {
   res.send("Welcome to the Blog_Platform API!!!!");
 });
 
+
+//sign up an account
 app.post('/signup', async (req, res) => {
   const hashedPassword = await bcrypt.hash(req.body.password, 10);
 
@@ -49,6 +51,39 @@ app.post('/signup', async (req, res) => {
 });
 
 
+//login an account
+app.post('/login', async (req, res) => {
+  try {
+    // First, find the user by their username
+    const user = await User.findOne({ where: { username: req.body.username } });
+
+    if (user === null) {
+      // If the user isn't found in the database, return an 'incorrect credentials' message
+      return res.status(401).json({
+        message: 'Incorrect credentials',
+      });
+    }
+
+    // If the user is found, we then use bcrypt to check if the password in the request matches the hashed password in the database
+    bcrypt.compare(req.body.password, user.password, (error, result) => {
+      if (result) {
+        // Passwords match
+
+        res.status(200).json({
+          message: 'Logged in successfully',
+          user: {
+            username: user.username,
+          },
+        });
+      } else {
+        // Passwords don't match
+        res.status(401).json({ message: 'Incorrect credentials' });
+      }
+    });
+  } catch (error) {
+    res.status(500).json({ message: 'An error occurred during the login process' });
+  }
+});
 
 // Get all the posts
 app.get("/posts", async (req, res) => {
