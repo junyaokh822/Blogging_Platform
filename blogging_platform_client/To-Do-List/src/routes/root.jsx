d4e7116ac074128314e09540c5212e4456ba010a
Junyao <junyaokh822@gmail.com>
2023-07-17 21:36:26 -0400

    adding logout button for root.jsx

--- blogging_platform_client/To-Do-List/src/routes/root.jsx
+++ blogging_platform_client/To-Do-List/src/routes/root.jsx
@@ -10,6 +10,13 @@ export async function loader({ request }) {
   }
   return { currentUser: null };
 }
+
+export async function action({ request }) {
+  const response = await fetch("/api/auth/logout", {
+    method: "DELETE"
+  });
+  return null;
+}
   
 function Root() {
   const { currentUser } = useLoaderData();
@@ -19,6 +26,25 @@ function Root() {
     setCurrentUser(currentUser);
   }, [currentUser]);
 
+    return (
+      <div>
+        <nav className="bg-blue-900 h-14 flex justify-items-center justify-between">
+          <h2 className="flex items-center">
+            <Link className="text-2xl flex items-center gap-1 px-2" to="/">
+              To-Do-List
+            </Link>
+          </h2>
+          <div className="flex items-center pr-2">
+            <Form method="post">
+              <button type="submit">Logout</button>
+            </Form>
+          </div>
+        </nav>
+        <div className={outletClasses}>
+          <Outlet />
+        </div>
+      </div>
+    );
 }
 
   export default Root;
