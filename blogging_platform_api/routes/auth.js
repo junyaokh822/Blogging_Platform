5550768faaab3b102ec6b895beece250b498fb09
Junyao <junyaokh822@gmail.com>
2023-07-17 21:12:26 -0400

    create auth.js

---
+++ blogging_platform_api/routes/auth.js
@@ -0,0 +1,16 @@
+const { User } = require("../models");
+
+router.get("/current_user", async (req, res) => {
+    if (req.session.userId) {
+      const user = await User.findByPk(req.session.userId);
+      return res.status(200).json({
+        user: {
+          id: user.id,
+          email: user.email,
+          name: user.name
+        }
+      });
+    } else {
+      return res.status(401).json({user: null})
+    }
+  });
